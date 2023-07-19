ad224782a63560a7fbab76b94056cfc8dea87e7e
elihl <elihl@LAPTOP-EVR5KTER>
2023-07-19 03:19:46 +0200

    Add project files.

---
+++ LocalFarmerCompany(2022)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalFarmerCompany_2022_
+{
+    
+    internal class Program
+    {
+        const double lessthan40kg = 5;
+        const double morethan80kg = 7.50;
+        const double More=8.50;
+        static void Main(string[] args)
+        {
+            double weight,total;
+            total = 0;
+          
+            Console.Write("Enter the produce weight:");
+            weight = double.Parse(Console.ReadLine());
+            if (weight<=40)
+            {
+                total = weight * lessthan40kg;
+            }
+            else if (weight >40 && weight <= 80)
+            {
+                total = weight * morethan80kg;
+            }
+            else
+            {
+                total = weight * More;
+            }
+            Console.WriteLine("{0:C}kg delivered, total due {1:C}",weight,total);
+            Console.ReadLine();
+
+        }
+    }
+}
